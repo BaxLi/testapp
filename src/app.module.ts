3712e280f3a1df2de21d18fdbd5c1928e3c0a471
BaxLi <lilian.baxan@gmail.com>
2021-10-10 10:08:14 +0300

    docker-compose yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '192.168.0.3',
+      port: 3306,
+      username: 'root',
+      password: 'Pass2021',
+      database: 'olanda',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
